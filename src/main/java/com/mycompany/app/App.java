8ec58d886ee669b9d22a49e0034340200324ef81
Vinh Lam <vlam@opswat.com>
2023-07-25 14:19:05 -0400

    initialize

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(StringUtils.upperCase("Hello World"));
+    }
+}
